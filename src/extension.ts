85a1f76c01d8c153664713a1187ec472b819fb0c
clarkio <clarkio@users.noreply.github.com>
2019-01-09 14:58:18 -0500

    WIP: commit it to winit (or save it for now)

--- src/extension.ts
+++ src/extension.ts
@@ -6,17 +6,20 @@ import * as path from "path";
 import {
   LanguageClient,
   LanguageClientOptions,
-  // SettingMonitor,
   ServerOptions,
   TransportKind
-} from "vscode-languageclient";
-import { Highlight } from "./highlight";
+} from "vscode-languageclient/lib/main";
+import { Highlighter } from "./highlighter";
 
-const decorationType = vscode.window.createTextEditorDecorationType({
+const highlightDecorationType = vscode.window.createTextEditorDecorationType({
   backgroundColor: "green",
   border: "2px solid white"
 });
-let highlights: Array<Highlight> = new Array<Highlight>();
+const emptyDecorationType = vscode.window.createTextEditorDecorationType({
+  backgroundColor: "transparent",
+  border: "0px"
+});
+let highlighters: Array<Highlighter> = new Array<Highlighter>();
 let client: LanguageClient;
 
 // this method is called when your extension is activated
@@ -37,7 +40,7 @@ export function activate(context: vscode.ExtensionContext) {
     documentSelector: ["*"],
     synchronize: {
       // Synchronize the setting section to the server
-      configurationSection: "twitchlinehighlighter"
+      configurationSection: "twitchhighlighter"
     }
   };
 
@@ -49,19 +52,125 @@ export function activate(context: vscode.ExtensionContext) {
 
   client.onReady().then(() => {
     client.onNotification("connected", () => {
-      vscode.window.showInformationMessage("TwitchLineHighlighter: Connected.");
+      console.log("We have begun connection with the Language Server");
+      vscode.window.showInformationMessage(
+        "Twitch Highlighter: Chat Listener Connected."
+      );
+    });
+    client.onNotification("error", (params: any) => {
+      console.log("Error handling in extension from client has been reached");
+      vscode.window.showErrorMessage(params.message);
+    });
+    client.onNotification("exited", () => {
+      vscode.window.showInformationMessage(
+        "Twitch Highlighter: Chat Listener Stopped"
+      );
+    });
+    client.onNotification("highlight", (params: any) => {
+      console.log(params);
+      executeHighlight(params.line, params.twitchUser);
+    });
+    client.onNotification("unhighlight", (params: any) => {
+      console.log(params);
+      removeHighlight(params.line, params.fileName);
     });
   });
 
-  vscode.commands.registerCommand("twitchlinehighlighter.start", () => {
-    vscode.window.showInformationMessage("TwitchLineHighlighter: Starting...");
-    client.start();
-  });
+  // #region command registrations
+  registerCommand(context, "twitchhighlighter.startChat", startChatHandler);
+  registerCommand(context, "twitchhighlighter.stopChat", stopChatHandler);
+  registerCommand(context, "twitchhighlighter.highlight", highlightHandler);
+  registerCommand(
+    context,
+    "twitchhighlighter.unhighlightSpecific",
+    unhighlightSpecificHandler
+  );
+  registerCommand(
+    context,
+    "twitchhighlighter.unhighlightAll",
+    unhighlightAllHandler
+  );
+  // #endregion command registrations
 
-  vscode.commands.registerCommand("twitchlinehighlighter.stop", () => {
-    vscode.window.showInformationMessage("TwitchLineHighlighter: Stopping...");
+  // #region command handlers
+  function highlightHandler() {
+    vscode.window
+      .showInputBox({ prompt: "Enter a line number" })
+      .then(executeHighlight);
+  }
+
+  function unhighlightAllHandler() {
+    vscode.window.visibleTextEditors.forEach(visibleEditor => {
+      visibleEditor.setDecorations(highlightDecorationType, []);
+    });
+    highlighters = new Array<Highlighter>();
+  }
+
+  function unhighlightSpecificHandler() {
+    let fileName: string;
+
+    vscode.window
+      .showInputBox({ prompt: "Enter a file name" })
+      .then(submittedFileName => {
+        if (!submittedFileName) {
+          vscode.window.showErrorMessage("A file name was not entered.");
+          return;
+        }
+        fileName = submittedFileName;
+        vscode.window
+          .showInputBox({ prompt: "Enter a line number" })
+          .then(lineNumber => {
+            if (!lineNumber || isNaN(+lineNumber)) {
+              vscode.window.showErrorMessage("Line number was not a number");
+              return;
+            }
+            const lineNumberInt = parseInt(lineNumber);
+            removeHighlight(lineNumberInt, fileName);
+          });
+      });
+  }
+
+  function startChatHandler() {
+    vscode.window.showInformationMessage(
+      "Twitch Highlighter: Starting Chat Listener..."
+    );
+    client.start();
+  }
+
+  function stopChatHandler() {
+    vscode.window.showInformationMessage(
+      "Twitch Highlighter: Stopping Chat Listener..."
+    );
     client.stop();
-  });
+  }
+  // #endregion command handlers
+
+  function executeHighlight(
+    lineNumber: string | undefined,
+    twitchUser: string = "self"
+  ) {
+    if (!lineNumber || isNaN(+lineNumber)) {
+      return;
+    }
+    const lineNumberInt: number = parseInt(lineNumber);
+
+    let editor = vscode.window.activeTextEditor;
+    if (editor) {
+      let doc = editor.document;
+      let existingHighlighter = highlighters.find(highlighter => {
+        return highlighter.editor.document.fileName === doc.fileName;
+      });
+      let range = getHighlightRange(lineNumber, doc);
+      let decoration = { range, hoverMessage: `From @${twitchUser}` };
+      addHighlight(
+        existingHighlighter,
+        decoration,
+        editor,
+        lineNumberInt,
+        twitchUser
+      );
+    }
+  }
 
   // Listen for active text editor so we don't lose any existing highlights
   let activeTextEditorListener = vscode.window.onDidChangeActiveTextEditor(
@@ -70,80 +179,90 @@ export function activate(context: vscode.ExtensionContext) {
         return;
       }
 
-      let existingHighlight = highlights.find(highlight => {
+      let existingHighlight = highlighters.find(highlight => {
         return (
           highlight.editor.document.fileName === activeEditor.document.fileName
         );
       });
       if (existingHighlight) {
         activeEditor.setDecorations(
-          decorationType,
+          highlightDecorationType,
           existingHighlight.decorations
         );
       }
     }
   );
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  let highlight = vscode.commands.registerCommand(
-    "twitchlinehighlighter.highlight",
-    () => {
-      vscode.window
-        .showInputBox({ prompt: "Enter a line number" })
-        .then(handleHighlight);
-    }
-  );
-
-  let unhighlightAll = vscode.commands.registerCommand(
-    "twitchlinehighlighter.unhighlightAll",
-    () => {
-      vscode.window.visibleTextEditors.forEach(visibleEditor => {
-        visibleEditor.setDecorations(decorationType, []);
-      });
-      highlights = new Array<Highlight>();
-    }
-  );
-
-  function handleHighlight(lineNumber: string | undefined) {
-    if (!lineNumber || isNaN(+lineNumber)) {
-      return;
-    }
-
-    let editor = vscode.window.activeTextEditor;
-    if (editor) {
-      let doc = editor.document;
-      let existingHighlight = highlights.find(highlight => {
-        return highlight.editor.document.fileName === doc.fileName;
-      });
-      let range = getHighlightRange(lineNumber, doc);
-      let decoration = { range };
-      addHighlight(existingHighlight, decoration, editor);
-    }
-  }
-
-  context.subscriptions.push(highlight);
-  context.subscriptions.push(unhighlightAll);
   context.subscriptions.push(activeTextEditorListener);
 }
 
 function addHighlight(
-  existingHighlight: Highlight | undefined,
-  decoration: { range: vscode.Range },
-  editor: vscode.TextEditor
+  existingHighlighter: Highlighter | undefined,
+  decoration: { range: vscode.Range; hoverMessage: string },
+  editor: vscode.TextEditor,
+  lineNumber: number,
+  twitchUser: string
 ) {
-  if (existingHighlight) {
-    existingHighlight.decorations.push(decoration);
-    editor.setDecorations(decorationType, existingHighlight.decorations);
+  if (existingHighlighter) {
+    // We have a new decoration for a highlight with decorations already in a file
+    // Add the decoration (a.k.a. style range) to the existing highlight's decoration array
+    // Reapply decoration type for updated decorations array in this highlight
+    existingHighlighter.decorations.push(decoration);
+    editor.setDecorations(
+      highlightDecorationType,
+      existingHighlighter.decorations
+    );
   } else {
-    highlights.push(new Highlight([decoration], editor, "clarkio"));
-    editor.setDecorations(decorationType, [decoration]);
+    // todo: for removing a highlight, find by linenumber, then update highlights array and editor.document decorations (how do we reset a single decoration and not all decorations?)
+    highlighters.push(
+      new Highlighter([decoration], editor, lineNumber, twitchUser)
+    );
+    editor.setDecorations(highlightDecorationType, [decoration]);
   }
 }
 
+function removeHighlight(lineNumber: number, fileName: string) {
+  fileName = `c:\\Users\\bc\\dev\\_repos\\simple-node-server\\${fileName}`;
+  const existingHighlight = findHighlighter(lineNumber, fileName);
+  if (!existingHighlight) {
+    // TODO: remove log before going to production
+    console.log(`Highlight not found so can't unhighlight the line from file`);
+    return;
+  }
+  const highlightIndex = highlighters.indexOf(existingHighlight);
+  highlighters.splice(highlightIndex, 1);
+  console.log(existingHighlight.decorations);
+  // As BraveCobra suggests we should maybe look into using the workspace.textDocuments instead.
+  // then use the findFile function to see if we can find it
+  // if it doesn't exist or maybe there's multiple we can prompt the user in chat or a whisper to specify which one (see csharpfritz stream from 01/04/2018 for an example in visual studio)
+  console.log(vscode.workspace.textDocuments);
+  let matchedFile;
+  vscode.workspace.findFiles(`**/${fileName}`).then(foundFiles => {
+    console.log("here are the found files!!!");
+    console.log(foundFiles);
+  });
+  const editor = vscode.window.visibleTextEditors.find(editor => {
+    return editor.document.fileName === fileName;
+  });
+  if (!editor) {
+    return;
+  }
+  editor.setDecorations(emptyDecorationType, existingHighlight.decorations);
+}
+
+function findHighlighter(
+  lineNumber: number,
+  fileName: string
+): Highlighter | undefined {
+  return highlighters.find(highlighter => {
+    return (
+      highlighter.lineNumber === lineNumber &&
+      highlighter.editor.document.fileName === fileName
+    );
+  });
+}
+
 function getHighlightRange(lineNumber: string, doc: vscode.TextDocument) {
-  // prefix string with plus to make string a number
+  // prefix string with plus (+) to make string a number
   // well at least that's what codephobia says :P
   const zeroIndexedLineNumber = +lineNumber - 1;
   // note: doc.lineAt is zero based index so remember to always do -1 from input
@@ -161,4 +280,13 @@ export function deactivate(): Thenable<void> {
     return Promise.resolve();
   }
   return client.stop();
+}
+
+function registerCommand(
+  context: vscode.ExtensionContext,
+  name: string,
+  handler: () => void
+) {
+  let disposable = vscode.commands.registerCommand(name, handler);
+  context.subscriptions.push(disposable);
 }
--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,12 @@ import {
   createConnection,
   IConnection,
   InitializeResult,
+  InitializedParams,
   TextDocumentSyncKind
-} from "vscode-languageserver";
+} from "vscode-languageserver/lib/main";
+
+const tmi = require("twitch-js");
+const ttvChatClient = new tmi.client(getTwitchChatOptions());
 
 let connection: IConnection = createConnection(
   new IPCMessageReader(process),
@@ -15,7 +19,6 @@ let connection: IConnection = createConnection(
 
 connection.onInitialize(
   (params): InitializeResult => {
-    connection.sendNotification("connected");
     // workspaceRoot = params.rootPath;
 
     return {
@@ -26,17 +29,98 @@ connection.onInitialize(
   }
 );
 
+connection.onInitialized((params: InitializedParams) => {
+  connection.sendNotification("connected");
+  if (ttvChatClient) {
+    ttvChatClient
+      .connect(getTwitchChatOptions())
+      .then(() => {
+        ttvChatClient.on("join", onTtvChatJoin);
+        ttvChatClient.on("chat", onTtvChatMessage);
+      })
+      .catch((error: any) => {
+        console.error("There was an issue connecting to Twitch");
+        console.error(error);
+        connection.sendNotification("error", {
+          message: "Server started but failed to connect to Twitch chat"
+        });
+      });
+  } else {
+    console.error("Twitch Chat Client not initialized");
+  }
+});
+
 connection.listen();
 
-const irc = require("irc");
+function onTtvChatJoin(channel: string, username: string, self: boolean) {
+  console.log(`[${username} has JOINED the channel ${channel}`);
+}
 
+function onTtvChatMessage(channel: string, user: any, message: string) {
+  const userName = user["display-name"] || user.username;
+  const lowerCaseMessage = message.toLowerCase();
+  console.log(`${userName}: ${lowerCaseMessage}`);
+  parseMessage(userName, message);
+}
+
+let highlighterCommands = ["!line", "!highlight"];
+let highlightCommandUsed: string;
+
+function parseMessage(userName: string, message: string) {
+  message = message.toLocaleLowerCase();
+  if (!isHighlightCommand(message)) {
+    return;
+  }
+
+  const chatMessageRawAction = message
+    .slice(highlightCommandUsed.length)
+    .trim();
+
+  const messageParts = chatMessageRawAction.split(" ");
+  if (messageParts.length === 0) {
+    // Example: !<command>
+    return;
+  }
+
+  // Possible formats to support:
+  // !<command> <line number> <default to currently open file>
+  // !<command> <line number> <filename.ts>
+  // !<command> <line number> <filename.ts>
+  // !<command> <line number> <filename.ts>
+  // !<command> <line number> <filename.ts>
+  if (messageParts.length === 1) {
+    // Example: !<command> <line number>
+    connection.sendNotification("highlight", {
+      line: messageParts[0],
+      twitchUser: userName
+    });
+  } else {
+    // Format Example: !<command> <line number> <filename.ts>
+    // Other Example: !<command> <line number> <filename.ts> <color>
+    connection.sendNotification("highlight", {
+      line: messageParts[0],
+      filename: messageParts[1],
+      twitchUser: userName
+    });
+  }
+}
+
+function isHighlightCommand(message: string) {
+  return highlighterCommands.some(
+    (command: string): boolean => {
+      const comparison = message.startsWith(command.toLowerCase());
+      highlightCommandUsed = comparison ? command : "";
+      return comparison;
+    }
+  );
+}
 // interface Settings {
-//   twitchlinehighlighter: TwitchLineHighlighterSettings;
+//   twitchhighlighter: TwitchHighlighterSettings;
 // }
 
 // These are the example settings we defined in the client's package.json
 // file
-interface TwitchLineHighlighterSettings {
+interface TwitchHighlighterSettings {
   connect: boolean;
   server: string;
   nickname: string;
@@ -45,48 +129,27 @@ interface TwitchLineHighlighterSettings {
   command: string;
   port: number;
 }
+connection.onShutdown(() => {
+  connection.sendNotification("exited");
 
-function connectIRC(config: TwitchLineHighlighterSettings) {
-  if (!config.connect) {
-    return () => {};
-  }
-
-  let ircConfig: any = {
-    channels: [config.channel],
-    port: config.port
-  };
-
-  if (config.password.length !== 0) {
-    ircConfig["password"] = config.password;
-  }
-
-  var client = new irc.Client(config.server, config.nickname, ircConfig);
-
-  client.addListener("error", function(message: string) {
-    console.log("error: ", message);
-  });
-
-  client.addListener("message", function(
-    from: string,
-    to: string,
-    message: string
-  ) {
-    if (to[0] !== "#") {
-      return;
-    }
-    console.log(`${from} says: "${message}"`);
-  });
-
-  return () => client.disconnect();
-}
-
-connection.onDidChangeConfiguration(change => {
-  console.log("Configuration change detected. Reconnecting...");
-  disconnect();
-  disconnect = connectIRC(settings);
+  ttvChatClient
+    .disconnect()
+    .then(() => {
+      console.debug(`Successfully disconnected from the Twitch chat`);
+    })
+    .catch((error: any) => {
+      console.error(`There was an error disconnecting from the Twitch chat`);
+      console.error(error);
+    });
 });
 
-let settings: TwitchLineHighlighterSettings = {
+connection.onDidChangeConfiguration(change => {
+  // console.log("Configuration change detected. Reconnecting...");
+  // disconnect();
+  // disconnect = connectIRC(settings);
+});
+
+let settings: TwitchHighlighterSettings = {
   connect: true,
   server: "irc.chat.twitch.tv",
   port: 80,
@@ -96,4 +159,18 @@ let settings: TwitchLineHighlighterSettings = {
   command: ":highlight"
 };
 
-let disconnect = connectIRC(settings);
+function getTwitchChatOptions() {
+  return {
+    channels: ["<your channel>"],
+    connection: {
+      reconnect: true
+    },
+    identity: {
+      password: "<your app password>"
+    },
+    options: {
+      clientId: "<your app client id>",
+      debug: false
+    }
+  };
+}
