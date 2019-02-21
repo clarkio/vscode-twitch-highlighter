4f639c0511299fb363bde2394be53d8feff9eb43
Anthony Conrad <8602418+parithon@users.noreply.github.com>
2019-02-20 20:17:03 -0800

    Removed mocha-junit-reporter and now use xunit

--- src/test/index.ts
+++ src/test/index.ts
@@ -17,9 +17,9 @@ import * as testRunner from 'vscode/lib/testrunner';
 testRunner.configure({
   ui: 'tdd', 		// the TDD UI is being used in extension.test.ts (suite, test, etc.)
   useColors: true, // colored output from test results
-  reporter: 'mocha-junit-reporter',
+  reporter: 'xunit',
   reporterOptions: {
-    mochaFile: '../TEST-RESULTS.xml'
+    output: '../TEST-RESULTS.xml'
   }
 });
 
